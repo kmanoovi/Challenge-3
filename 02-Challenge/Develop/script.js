986be73627241536c327c77b95de9accec052ae3
kmanoovi <ktmandovi@gmail.com>
2022-08-31 17:13:00 -0400

    added more variables and randomizers

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,11 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+console.log(upperCase)
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var number = "0123456789".split("");
+var specialCharacters = "!@#$%^&*?".split("");
+var masterSet = [];
+var builtPassword = "";
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generatePassword() {
+
+  var characterQuantity = window.prompt("How many characters do you want your password to be?")
+
+  var userChoiceOne = window.confirm("Do you want Upper Case letters in your password?");
+  console.log(userChoiceOne)
+
+  var userChoiceTwo = window.confirm("Do you want to use Lower Case letters in your password?")
+
+  var userChoiceThree = window.confirm("Do you want Numbers in your password?");
+
+  var userChoiceFour = window.confirm("Do you want Special Characters in your password?");
+
+  if (userChoiceOne == true) {
+    masterSet = masterSet.concat(upperCase)
+    console.log(masterSet)
+  }
+  
+  if (userChoiceTwo == true) {
+    masterSet = masterSet.concat(lowerCase)
+    console.log(masterSet)
+  }
+
+  if (userChoiceThree == true) {
+    masterSet = masterSet.concat(number)
+    console.log(masterSet)
+  }
+
+  if (userChoiceFour == true) {
+    masterSet = masterSet.concat(specialCharacters)
+    console.log(masterSet)
+  }
+
+  for (var i = 1; i < parseInt(characterQuantity); i++) {
+    let randomIndex = getRandomNumber(0, masterSet.length -1);
+    let randomChar = masterSet[randomIndex];
+    builtPassword += randomChar;
+  
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
@@ -13,59 +64,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var userYes = true;
-var userNo = false;
-
-var userChoiceOne = window.prompt ("Do you want Upper Case letters in your password?");
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// userChoice = userChoiceOne.toUpperCase();
-
-var userChoiceTwo = window.prompt ("Do you want to use Lower Case letters in your password?");
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-// userChoice = userChoiceTwo.toLowerCase();
-
-var userChoiceThree = window.prompt ("Do you want Numbers in your password?");
-// var options = [Math.floor(Math.random() * options.length)];
-// userChoice = userChoiceThree.Math.options
-
-var userChoiceFour = window.prompt ("Do you want Special Characters in your password?");
-var specialCharacters = "!@#$%^&*?";
-// userChoice = userChoiceFour.specialCharacters
-
-var index = Math.floor(Math.random() * options.length);
-var computerChoice = options[index];
-
-// Trying to figure out how to get between 8 and 128 characters.
-// for (var i = 8; i < 128)
-
-if (userChoiceOne == userYes) {
-  upperCase()
-} else if (userChoiceOne !== userYes) {
-  DO_NOTHING
-};
-
-if (userChoiceTwo == userYes) {
-  lowerCase()
-} else if (userChoiceTwo !== userYes) {
-  DO_NOTHING
-};
-
-if (userChoiceThree == userYes) {
-  optios()
-} else if (userChoiceThree !== userYes) {
-  DO_NOTHING
-};
-
-if (userChoiceFour == userYes) {
-  specialCharacters()
-} else if (userChoiceFour !== userYes) {
-  DO_NOTHING
-};
 
 
-// if (userChoiceFour == true) {
-//   Math.floor(Math.random() * 11)
-// } else if (userChoiceFour !== true) {
-//   DO_NOTHING
-// };
+
 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Challenge-3
+# Generate Random Password Debug
+
+I began this code by defining variables for all the characters that could be selected
+for the random password.
+
+I then wrote out all the prompts and messages I wanted the user to have to either confirm or deny
+so the script knew which types of characters to add to the random password.
+
