9fb01320618266ab46aac02174d88e615fcf79d4
kmanoovi <ktmandovi@gmail.com>
2022-08-31 08:02:15 -0400

    added if statements

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -13,32 +13,55 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var userChoiceOne = window.prompt ("Do you want Upper Case letters in your password?")
+var userYes = true;
+var userNo = false;
+
+var userChoiceOne = window.prompt ("Do you want Upper Case letters in your password?");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // userChoice = userChoiceOne.toUpperCase();
 
-var userChoiceTwo = window.prompt ("Do you want to use Lower Case letters in your password?")
+var userChoiceTwo = window.prompt ("Do you want to use Lower Case letters in your password?");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 // userChoice = userChoiceTwo.toLowerCase();
 
-var userChoiceThree = window.prompt ("Do you want Numbers in your password?")
+var userChoiceThree = window.prompt ("Do you want Numbers in your password?");
 // var options = [Math.floor(Math.random() * options.length)];
 // userChoice = userChoiceThree.Math.options
 
-var userChoiceFour = window.prompt ("Do you want Special Characters in your password?")
-
-
-// var userChoiceOne = window.prompt ("");
-// var upperCase = window.prompt ();
-
-// var userChoiceTwo
-// var lowerCase = window.prompt ();
-
-// var userChoiceThree
-// var specialCharacters = window.prompt ();
-
-// var userChoiceFour = window.prompt ("Do you want to have numbers in your password?")
+var userChoiceFour = window.prompt ("Do you want Special Characters in your password?");
+var specialCharacters = "!@#$%^&*?";
+// userChoice = userChoiceFour.specialCharacters
 
 var index = Math.floor(Math.random() * options.length);
 var computerChoice = options[index];
+
+// Trying to figure out how to get between 8 and 128 characters.
+// for (var i = 8; i < 128)
+
+if (userChoiceOne == userYes) {
+  upperCase()
+} else if (userChoiceOne !== userYes) {
+  DO_NOTHING
+};
+
+if (userChoiceTwo == userYes) {
+  lowerCase()
+} else if (userChoiceTwo !== userYes) {
+  DO_NOTHING
+};
+
+if (userChoiceThree == userYes) {
+  optios()
+} else if (userChoiceThree !== userYes) {
+  DO_NOTHING
+};
+
+if (userChoiceFour == userYes) {
+  specialCharacters()
+} else if (userChoiceFour !== userYes) {
+  DO_NOTHING
+};
+
 
 // if (userChoiceFour == true) {
 //   Math.floor(Math.random() * 11)
